da1ccdf8ff3df154b552c5736d548743a64ca25a
MW <123622276+wm155600@users.noreply.github.com>
2024-03-21 07:37:40 +0000

    filme

--- Kino/abfragen.sql
+++ Kino/abfragen.sql
@@ -1,3 +1,4 @@
 SELECT * FROM Film;
+
 SELECT * FROM Regisseur;
 
--- Kino/insertKino.sql
+++ Kino/insertKino.sql
@@ -13,3 +13,25 @@ INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES
 ('Der Pate', 175, 1972, 3),          -- Francis Ford Coppola
 ('Der Marsianer', 151, 2015, 4),     -- Ridley Scott
 ('Intime', 109, 2011, 5);            -- Michel Hazanavicius
+
+DELETE FROM Film WHERE FilmID >5
+
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Aquaman', 143, 2018, 2);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Apocalypse Now', 153, 1979, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Harry Potter 2', 161, 2002, 1);
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Blade Runner', 117, 1982, 4);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Artist', 100, 2011, 5);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Harry Potter 3', 142, 2004, 1);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Godfather Part II', 200, 1974, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Godfather Part III', 162, 1990, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Alien', 117, 1979, 4); -- obwohl es ein Sci-Fi Horrorfilm ist, könnte er aufgrund seiner historischen Bedeutung und künstlerischen Qualität für ältere Jugendliche geeignet sein
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Gladiator', 155, 2000, 4);
